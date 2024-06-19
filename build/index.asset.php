0033c107013ca96e723d3ac52d2e7a1fffaa83e2
DevDossett <github@dossett.dev>
2024-06-19 00:15:37 -0500

    Cleaned up block.json

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '78317763cfe110e8c557');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'ee9802774e889af81d26');
